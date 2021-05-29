9e576bcd1ea67ee04db1dc828af56ba8d530ff7a
fatimagarcia1030 <84999309+fatimagarcia1030@users.noreply.github.com>
2021-05-29 12:50:31 +1200

    Adding Page Model Class

---
+++ BaseProject/PageModels/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using BaseProject.Extensions;
+
+namespace BaseProject.PageModels
+{
+    public class Class1
+    {
+        private IWebDriver Driver;
+
+        private IWebElement _elementName;
+        public Class1(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebElement ElementName
+        {
+            get
+            {
+                _elementName = Driver.FindElement(20, By.XPath("xpathvalue"));
+                return _elementName;
+            }
+        }
+    }
+}
